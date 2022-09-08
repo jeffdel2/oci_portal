c26c7b6e7df53360f668d5afdfd40e135fd00c18
Glitch (periodic-steady-jump) <none>
2022-09-08 17:00:26 +0000

    🌯🗽 Checkpoint ./app.js:36786028/53

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ axios
           accessToken, refreshToken, params
         }, null, 2)}\n*****`);
         id_token = idToken;
-        return done(null, profile, context, idToken, accessToken, refreshToken, params);
+        return done(null, profile);
       }));
     }
     else {
