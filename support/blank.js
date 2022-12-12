c11e7996f7dc0ced97d9a9857d8140599f502128
Glitch (periodic-steady-jump) <none>
2022-12-12 19:22:55 +0000

    🌅💰 Checkpoint ./support/blank.js:36786028/5

--- support/blank.js
+++ support/blank.js
@@ -51,11 +51,12 @@ function callPublicAPI() {
 */
 console.log('Client-side code running');
 
+/*
 const button = document.getElementById('publicButton');
 button.addEventListener('click', function(e) {
   console.log('button was clicked');
 });
-
+*/
 
 $('#publicButton').click(function(){
     console.log('button clicked');
