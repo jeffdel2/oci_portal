65b96f39ba1a92f746a2ab04720fe4d664c49de8
Glitch (periodic-steady-jump) <none>
2022-12-14 19:00:38 +0000

    🔑✈️ Checkpoint ./support/blank.js:36786028/12

--- support/blank.js
+++ support/blank.js
@@ -45,7 +45,7 @@ const callApi = async (baseUrl, endpoint) => {
   try {
     //const token = await auth0.getTokenSilently();
     var baseUrl = "https://okta-rocks-api.glitch.me/";
-    console.log(baseUrl);
+    console.log("checking", baseUrl);
     var endpoint = "/api/private";
     var token = "1111";
     const response = await fetch(baseUrl + endpoint, {
