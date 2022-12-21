92655c660a7e14a5233b57443bb3e429d47e6121
Glitch (periodic-steady-jump) <none>
2022-12-21 15:30:48 +0000

    👱💨 Checkpoint ./support/client.js:36786028/1312

--- support/client.js
+++ support/client.js
@@ -1,14 +1,11 @@
 /* Client support JS goes here */
 
-//const app = require('./app.js');
-//import { app } from './app.js';
+//const {app} = require('./app.js');
 
-
-//require('dotenv').config({ path: '.okta.env' })
-//const { endpoint, baseUrl } = process.env;
+//console.log("Access token: ", app.am_token);
 
 var baseAPIUrl = "https://okta-rocks-api.glitch.me/"; // This is the base URL of the API Application
-var token = "eyJraWQiOiI3Y3N6LUpqN2pDVXh6MjctN3QxVWJaWTY3T2g1aVlmSlBaZjVKYTBWR05RIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULk1rUkhhSTFPOTBQUjU3RFJ4Mi1TeGhHR2I3bUI3ZDRWQVhOZlNyZVA2WFEiLCJpc3MiOiJodHRwczovL2RlbG9uZ29pZS5va3RhcHJldmlldy5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNjcxMTMwODc5LCJleHAiOjE2NzExMzQ0NzksImNpZCI6IjBvYTV4d3ZkcDB3Rmg2ZU81MWQ3IiwidWlkIjoiMDB1Mml3bmp3cVAyREhOYlMxZDciLCJzY3AiOlsib3BlbmlkIiwiZ3JvdXBzIiwicGhvbmUiLCJwcm9maWxlIl0sImF1dGhfdGltZSI6MTY3MTEzMDg3OCwic3ViIjoiand0QGF0a28uZW1haWwifQ.L_J96IXhOD0B0BJhvCl3eMpUK6oxaYvPmRrLh2wqG6ITPvX-cTh1LLJrf7XztT8maOkdntrHwqoEerlmvda-2-LPhk-T-QWohvilsFON1XWZAVzcmQMqi77CUf8N1yNFJpKM9OIWR8Xrr2zG2qKBbUbACCDDnqIx01ho4YL55CSZ9DJnHqBxm5i-tYXr-Ua1lpLdqv__ubIRhhQZCmtCO9Fq5XALhYNISJuTpnbnjYOddvfRG5XwsL3bPY31N2VczPHfxyCrR7kKaup5fffsWb9KNW60-Y9JgdQtzE8IOjRgBiDXQc-dnD18TmbJ5SpHQa7xERkOcruDlcmRVZ1rSA";
+var token = amtoken;
 
 
 console.log('Client-side code');
