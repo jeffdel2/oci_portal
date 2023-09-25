f9c1992ee619015f88d2bc5d1e76bcbccb5c661e
Glitch (periodic-steady-jump) <none>
2023-09-25 18:49:30 +0000

    🎷🏩 Checkpoint ./support/client.js:36786028/101

--- support/client.js
+++ support/client.js
@@ -14,7 +14,7 @@ Set API base URL
 */
 var baseAPIUrl = "https://acme-api.glitch.me/"; // This is the base URL of the API Application
 var baseOrgUrl = "https://acme.j5demo.com/";
-console.log("MAIN COOKIE",cookieValue);
+console.log("MAIN COOKIE",document);
 
 
 /*
