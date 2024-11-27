6ab1507fd2d882409d3d2076b61db82996990373
Glitch (periodic-steady-jump) <none>
2024-11-27 21:13:08 +0000

    🕴🌹 Checkpoint ./app.js:36786028/223

--- app.js
+++ app.js
@@ -187,12 +187,9 @@ app.use('/forgotusername', (req, res) => {
 app.use((req, res, next) => {
   if (req.isAuthenticated()) {
     const obj = JSON.parse(decoded_id_token);
-    console.log("NEW", obj.groups[1]);
     const groups = obj.groups;
-    console.log("TESTING", groups.indexOf("*Admin*"));
-    //req.isAdmin = groups.includes('Admin'); // Check if user is in the "Admin" group
     req.isAdmin = groups.some(group => group.includes('Admin'));
-    req.isUser = groups.some(group => group.includes('User'));   // Check if user is in the "User" group
+    req.isUser = groups.some(group => group.includes('User'));
   } else {
     req.isAdmin = false;
     req.isUser = false;
